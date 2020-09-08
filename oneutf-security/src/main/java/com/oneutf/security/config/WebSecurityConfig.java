c4b50aecd0a0a24548211a17f1d3ff3e9362174e
oneutf <oneutf>
2020-09-08 20:02:33 +0800

    修改框架

--- oneutf-security/src/main/java/com/oneutf/security/config/WebSecurityConfig.java
+++ oneutf-security/src/main/java/com/oneutf/security/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().anonymous().and().csrf().disable();
+        http.authorizeRequests().anyRequest().anonymous();
         http.cors().disable();
     }
 
