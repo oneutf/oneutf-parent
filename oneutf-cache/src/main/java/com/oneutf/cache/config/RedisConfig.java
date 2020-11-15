9a9f53849a7ab89e7f687d3aa4f956b559d3b014
oneutf <2035734975@qq.com>
2020-11-15 12:24:42 +0800

    1

--- oneutf-cache/src/main/java/com/oneutf/cache/config/RedisConfig.java
+++ oneutf-cache/src/main/java/com/oneutf/cache/config/RedisConfig.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
+/**
+ * @author oneutf
+ */
 @Configuration
 public class RedisConfig {
 
